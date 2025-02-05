28f78967dd9635a80d6475aba59ae663bcbeba46
Hafsa Egal <hafsaegal915@gmail.com>
2025-02-05 13:14:02 +0300

    js updated

--- script/script.js
+++ script/script.js
@@ -2,4 +2,13 @@ document
   .getElementById("appointmentForm")
   .addEventListener("submit", function (event) {
     event.preventDefault();
-  });
+
+// Get form input
+const patientName =document.getElementById('patientName')
+const email = document.getElementById("email");
+const phoneNo = document.getElementById("phoneNo");
+const specialist = document.getElementById("specialist");
+const appointmentDate = document.getElementById("appointmentDate");
+const appointmentTime = document.getElementById("appointmentTime");
+ });
+
